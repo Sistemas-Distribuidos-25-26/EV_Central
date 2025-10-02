d0783f25e03da3ed492863c02235f1e0dfd4efc0
Marcos González <imlecus@proton.me>
2025-10-02 12:29:19 +0200

    Added SQLite database

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from sys import argv
 from socket import socket
+import sqlite3 as sqlite
 
 ARG_NUMBER = 0
 
@@ -9,6 +10,19 @@ if len(argv) < ARG_NUMBER:
 
 PORT = int(argv[1])
 
+def create_database(filename: str):
+    try:
+        with sqlite.connect(filename) as c:
+            cursor = c.cursor()
+            cursor.execute("""CREATE TABLE IF NOT EXISTS drivers (id INT PRIMARY KEY);""")
+            cursor.execute("""CREATE TABLE IF NOT EXISTS charging_points (id INT PRIMARY KEY, x REAL NOT NULL, y REAL NOT NULL, state TEXT NOT NULL);""")
+            c.commit()
+            print("Base de datos creada o restaurada")
+            return cursor
+
+    except sqlite.DatabaseError as e:
+        print(e)
+
 def create_server_socket(port: int) -> socket :
     s = socket()
     s.bind(('', PORT))
@@ -17,6 +31,7 @@ def create_server_socket(port: int) -> socket :
     return s
 
 
+create_database("database.db")
 server = create_server_socket(PORT)
 while True:
     connection, addr = server.accept()
