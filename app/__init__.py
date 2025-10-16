9d49112d2328f80ae2460b1066ac6bd59d1b20be
Marcos <mgf108@alu.ua.es>
2025-10-16 23:05:51 +0200

    Added standard sockets protocol

--- app/__init__.py
+++ app/__init__.py
@@ -4,17 +4,50 @@ from database import db
 from gui import run
 import threading
 
+STX = b'\x02'
+ETX = b'\x03'
+EOT = b'\x04'
+ENQ = b'\x05'
+ACK = b'\x06'
+NACK = b'\x15'
+
+def calculate_lrc(data):
+    lrc = 0
+    for byte in data:
+        lrc ^= byte
+    return bytes([lrc])
+
+
 def handle_monitor(connection):
-    while True:
-        message = connection.recv(1024).decode()
-        if not message:
-            print("[ServerSocket] El cliente ha cortado la conexión")
-            break
-        print(f"[ServerSocket] Recibido: {message}")
-        id, state = message.split(',')
-        if not db.exists(id):
-            print(f"[ServerSocket] Alta de nuevo Charging Point {id}")
-            db.add_cp(id, 0, 0, "Nombre", 0, state)
+    try:
+        while True:
+            enquiry = connection.recv(1)
+            connection.send(ACK if enquiry == ENQ else NACK)
+            while True:
+                message = connection.recv(1024)
+                if not message or message == EOT:
+                    print("[ServerSocket] El cliente ha cortado la conexión")
+                    return
+                if message[0:1] != STX or ETX not in message:
+                    connection.send(NACK)
+                    break
+
+                end = message.index(ETX)
+                data = message[1:end]
+                lrc = message[end+1:end+2]
+                if lrc != calculate_lrc(data):
+                    connection.send(NACK)
+                else:
+                    connection.send(ACK)
+                    print(f"[ServerSocket] Recibido: {data.decode()}")
+                    id, state = data.decode().split(',')
+                    if not db.exists(id):
+                        print(f"[ServerSocket] Alta de nuevo Charging Point {id}")
+                        db.add_cp(id, 0, 0, "Nombre", 0, state)
+    except ConnectionError:
+        print("[ServerSocket] El cliente ha cortado la conexión")
+        connection.close()
+
 
 def run_server_socket() -> None :
     s = socket()
--- app/gui.py
+++ app/gui.py
@@ -1,5 +1,5 @@
 import dash
-from dash import html, Input, Output
+from dash import html, Input, Output, dcc
 import dash_bootstrap_components as dbc
 import dash_leaflet as dl
 from database import db
@@ -14,11 +14,21 @@ header = dbc.Navbar(
     fixed="top"
 )
 
-def cp_widget(id: str, state: str) -> html.Div:
+COLOR_MAP = {
+    "UNKNOWN": "#b0a7b8",
+    "ACTIVE": "#9de64e",
+    "OUT_OF_ORDER": "#de5d3a",
+    "CHARGING":"#3388de",
+    "BROKEN": "#ec273f",
+    "DISCONNECTED": "#b0a7b8"
+}
+
+def cp_widget(id: str, state: str, price: str) -> html.Div:
     return html.Div([
         html.H3(id),
-        html.P(f"Estado: {state}")
-    ], className="cp")
+        html.P(f"Estado: {state}"),
+        html.P(f"{price}€/kWh")
+    ], className="cp", style={"background-color": COLOR_MAP.get(state, "white")})
 
 cps = db.get_cps()
 
@@ -32,46 +42,72 @@ for cp in cps:
         }
     )
 
-
 widgets = []
 for cp in cps:
-    widgets.append(cp_widget(cp[0], cp[5]))
+    widgets.append(cp_widget(cp[0], cp[5], cp[4]))
 
 sidebar = html.Aside([
     html.H5("Menú", className="display-6"),
     html.Hr(),
-    html.P("Aquí se añadirán elementos futuros", className="lead"),
-    *widgets
-])
+    html.Div([*widgets], id="widget-sidebar")
+], id="widget-panel")
 
 markers = [dl.Marker(position=pt["location"], children=dl.Tooltip(pt["name"])) for pt in points]
 
 mapa = dl.Map(center=[40.4168, -3.7038], zoom=13, children=[
     dl.TileLayer(),
     dl.LayerGroup(markers)
-], style={"width": "100%", "height": "600px"})
+], style={"width": "70%", "height": "800px"}, id="map")
 
-content = html.Div([
-    html.H3("Mapa de Puntos de Recarga"),
-    mapa,
-], id="content")
+control_panel = html.Aside([
+    html.P("Peticiones en curso"),
+    html.P("Mensajes internos"),
+    html.Pre("")
+], id="control-panel")
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP], assets_folder="assets")
 
 app.layout = html.Div([
     header,
     sidebar,
-    content,
+    mapa,
+    control_panel,
+    dcc.Interval(interval=1000, id="interval-component", n_intervals=0)
 ])
 
 @app.callback(
-    Output("search-input", "value"),
-    Input("search-input", "value"),
+    Output("widget-sidebar", "children"),
+    Input("interval-component", "n_intervals")
 )
-def search_ev_points(query):
-    if query:
-        print(f"Búsqueda: {query}")
-    return dash.no_update
+def refresh_cps(n):
+    global widgets
+    global cps
+    widgets = []
+    cps = db.get_cps()
+    for cp in cps:
+        widgets.append(cp_widget(cp[0], cp[5], cp[4]))
+    return widgets
+
+
+@app.callback(
+    Output("widget-sidebar", "children"),
+    [Input("search-input", "value")]
+)
+def search_ev_points(query: str):
+    global widgets
+    print(f"Búsqueda: {query}")
+
+    widgets = []
+    if query == "" or query is None:
+        for cp in cps:
+            widgets.append(cp_widget(cp[0], cp[5], cp[4]))
+    else:
+        for cp in cps:
+            if str(cp[0]).find(query.upper()) != -1:
+                widgets.append(cp_widget(cp[0], cp[5], cp[4]))
+    if not widgets:
+        widgets = [html.P("No hay resultados")]
+    return widgets
 
 def run():
     app.run()
