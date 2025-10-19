a82ac605455336c2ac0cb667aa6f4a13128cc905
Marcos <mgf108@alu.ua.es>
2025-10-19 18:28:37 +0200

    Added Kafka configuration scripts

---
+++ kafka_setup.sh
@@ -0,0 +1,9 @@
+echo "Inicializando Kafka..."
+docker start ev_kafka
+sleep 5
+echo "Instalando Kafka..."
+docker exec ev_kafka /opt/kafka/bin/kafka-topics.sh --create --topic requests --partitions 1 --replication-factor 1 --bootstrap-server localhost:9092
+docker exec ev_kafka /opt/kafka/bin/kafka-topics.sh --create --topic notifications --partitions 1 --replication-factor 1 --bootstrap-server localhost:9092
+docker exec ev_kafka /opt/kafka/bin/kafka-topics.sh --create --topic orders --partitions 1 --replication-factor 1 --bootstrap-server localhost:9092
+echo "Se han creado los siguientes topics:"
+docker exec ev_kafka /opt/kafka/bin/kafka-topics.sh --list --bootstrap-server localhost:9092
